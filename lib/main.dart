2f5cbb89b75fb27e504b442827226a8cc44a0131
Z-kamili <zakariakamili97@gmail.com>
2022-10-28 23:43:05 +0100

    add transaction

--- lib/main.dart
+++ lib/main.dart
@@ -30,53 +30,46 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+
+  final List<>;
+
   const MyHomePage({super.key, required this.title});
 
   final String title;
+
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+
+
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget> [
-               Card(
-                  color: Colors.blue,
-                  child: Container(
-                    width:double.infinity, 
-                    child : Text('CHART!')
-                  ), 
-                  elevation: 5,
+            Container(
+                width: double.infinity,
+                child: Card(
+                  color : Colors.blue,
+                  child : Text('CHART!'),
+                  elevation : 5,
                ),
-               Card(child: Text('LIST OF TX'),),
+            ),
+            Card(
+                color: Colors.red,
+                child: Text('LIST OF TX'),
+                ),
             ],
         ),
     );
