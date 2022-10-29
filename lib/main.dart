d9951339ccd2c71befe80515eebe16e9ab7c3469
Z-kamili <zakariakamili97@gmail.com>
2022-10-29 02:18:13 +0100

    add style

--- lib/main.dart
+++ lib/main.dart
@@ -83,16 +83,24 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                     decoration: BoxDecoration(
                       border:Border.all(
-                        color: Colors.black,
+                        color: Colors.purple,
                         width: 2,
                       ),
                     ),
                     padding: EdgeInsets.all(10),
                     child:Text(
                       tx.amount.toString(),
+                      style:TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple
+                        ),
+
                       ),
                     ),
-                    Column(children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
                       Text(tx.title),
                       Text(tx.date.toString()),
                     ],)
