1a129a90b56a4ba9614126125c7dd6b4ed6b05e3
Z-kamili <zakariakamili97@gmail.com>
2022-10-31 09:25:44 +0000

    add transaction list

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:first_app/models/transaction.dart';
-import './widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 void main() {
@@ -121,44 +120,6 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
               )
           ),
-           Column(
-            children: transaction.map((tx) {
-              return Card(
-                  child : Row(children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                       vertical: 10,
-                       horizontal: 15,
-                    ),
-                    decoration: BoxDecoration(
-                      border:Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      ),
-                    ),
-                    padding: EdgeInsets.all(10),
-                    child:Text(
-                     'A: ${tx.amount.toString()}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                      Text(tx.title,style:TextStyle(fontSize: 18,fontWeight:FontWeight.bold)),
-                      Text(
-                      DateFormat('yyyy-MM-dd').format(tx.date),
-                      style: TextStyle(color: Colors.grey),
-                      ),
-                    ],)
-              ],),
-              );
-            }).toList(),
-           ),
         ],
     ),
   );
