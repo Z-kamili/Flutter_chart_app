f21c99251962bb93ebf63603f1bcb1dc8c361999
Z-kamili <zakariakamili97@gmail.com>
2022-11-05 15:48:25 +0000

    bug fixed

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,60 +12,57 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-            return Container(
-              height: 300,
-              child : transaction.isEmpty ? Column(children: <Widget>[
-                 Text(
-                  'No Transactions added yet',
-                  style: Theme.of(context).textTheme.titleLarge,
+    return Container(
+      height: 300,
+      child: transaction.isEmpty
+          ? Column(
+              children: <Widget>[
+                Text(
+                  'No transactions added yet!',
+                  // style: Theme.of(context).textTheme.title,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    height: 200,
+                    child: Image.asset(
+                      'assets/image/waiting.png',
+                      fit: BoxFit.cover,
+                    )),
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
                   ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                Container(height: 200, child : Image.asset('assets/image/waiting.png',fit:BoxFit.cover))  
-              ],) : ListView.builder(
-              itemBuilder: (ctx,index){
-              return Card(
-                  child : Row(children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                       vertical: 10,
-                       horizontal: 15,
-                    ),
-                    decoration: BoxDecoration(
-                      border:Border.all(
-                        color: Theme.of(context).primaryColor,
-                        width: 2,
-                      ),
-                    ),
-                    padding: EdgeInsets.all(10),
-                    child:Text(
-                     'A: ${transaction[index].amount.toString()}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Theme.of(context).primaryColor,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text('\$${transaction[index].amount}'),
                         ),
                       ),
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                      Text(transaction[index].title,style:
-                      Theme.of(context).textTheme.titleLarge
-                      ),
-                      Text(
-                         DateFormat('yyyy-MM-dd').format(transaction[index].date),
-                         style: TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                  )
-              ],),
-              );
+                    title: Text(
+                      transaction[index].title,
+                      // style: Theme.of(context).textTheme.title,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(transaction[index].date),
+                    ),
+                  ),
+                );
               },
               itemCount: transaction.length,
-           ),
-        );
+            ),
+    );
 
 
   }
